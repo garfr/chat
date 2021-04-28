7d764b73532afc3e18bb378476762b53c76649f0
garfr <gavratcliff@gmail.com>
2021-04-28 18:07:37 -0500

    feat: properly polls stdout to print, avoiding blocking

--- client/main.c
+++ client/main.c
@@ -10,7 +10,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define NUM_FDS 2
+#define NUM_FDS 3
 #define IN_MESSAGE_MAX 1000
 #define OUT_MESSAGE_MAX 1000
 
@@ -70,14 +70,18 @@ int main(int argc, char* argv[]) {
 
   pfds[0].fd = 0;
   pfds[0].events = POLLIN;
-  pfds[1].fd = conn_fd;
-  pfds[1].events = POLLIN | POLLOUT;
+  pfds[1].fd = 1;
+  pfds[1].events = POLLOUT;
+  pfds[2].fd = conn_fd;
+  pfds[2].events = POLLIN | POLLOUT;
 
-  int has_message = 0;
+  int message_prepared = 0;
+  int message_received = 0;
+
   uint8_t stdin_msg[IN_MESSAGE_MAX];
-  size_t stdin_msg_len;
+  ssize_t stdin_msg_len;
   uint8_t server_msg[IN_MESSAGE_MAX];
-  size_t server_msg_len;
+  ssize_t server_msg_len;
   (void)server_msg_len;
 
   /*uint8_t out_msg[IN_MESSAGE_MAX];*/
@@ -91,21 +95,29 @@ int main(int argc, char* argv[]) {
     /* stdin */
     if (pfds[0].revents & POLLIN) {
       stdin_msg_len = read(0, stdin_msg, IN_MESSAGE_MAX);
-      has_message = 1;
+      message_prepared = 1;
       verbose_log("Read message from stdin.\n");
     }
+    if ((pfds[1].revents & POLLOUT) && message_received) {
+      write(1, server_msg, server_msg_len);
+      message_received = 0;
+    }
     /* server */
-    if (pfds[1].revents & POLLIN) {
-      server_msg_len = recv(pfds[1].fd, server_msg, IN_MESSAGE_MAX, 0);
+    if (pfds[2].revents & POLLIN) {
+      server_msg_len = recv(pfds[2].fd, server_msg, IN_MESSAGE_MAX, 0);
       if (server_msg_len == 0) {
         verbose_log("Connection with server lost.\n");
         goto server_closed;
+      } else if (server_msg_len == -1) {
+        printf("Error reading from server: %s.\n", strerror(errno));
+        goto server_closed;
       }
       verbose_log("Read message from server %d.\n", server_msg_len);
+      message_received = 1;
     }
-    if (pfds[1].revents & POLLOUT && has_message) {
-      send(pfds[1].fd, stdin_msg, stdin_msg_len, 0);
-      has_message = 0;
+    if ((pfds[2].revents & POLLOUT) && message_prepared) {
+      send(pfds[2].fd, stdin_msg, stdin_msg_len, 0);
+      message_prepared = 0;
       verbose_log("Sending message to server.\n");
     }
   }
